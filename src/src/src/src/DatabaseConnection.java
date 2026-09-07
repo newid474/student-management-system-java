4e5e1a4e4ab76d9567587e75bcbee80902b868ed
Sangipogu Aishu <sangipoguaishu@gmail.com>
2026-09-07 10:57:33 +0530

    Add database connection

---
+++ src/src/src/src/DatabaseConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/student_management";
+
+    private static final String USER = "root";
+
+    // Change this to your MySQL password
+    private static final String PASSWORD = "YOUR_PASSWORD";
+
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
